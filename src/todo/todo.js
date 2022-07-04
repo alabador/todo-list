5deda58b2068cc2e5561eaed6b9304239c298b98
alabador <austinlabador@gmail.com>
2022-07-04 00:26:54 -0700

    Add properties and create todo class

---
+++ src/todo/todo.js
@@ -0,0 +1,15 @@
+
+
+class Todo {
+    constructor(title, description, dueDate, priority, notes, subtasks, tags){
+        this.title = title,
+        this.description = description,
+        this.dueDate = dueDate,
+        this.priority = priority,
+        this.notes = notes,
+        this.subtasks = subtasks,
+        this.tags = tags
+    }
+    edit();
+    delete();
+}
