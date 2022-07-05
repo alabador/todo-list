28f3a59754a1a8a83cc2aef95796cd9556eb812f
alabador <austinlabador@gmail.com>
2022-07-04 19:07:08 -0700

    Added different modules

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,13 +1,44 @@
 
 
 class Task {
-    constructor(title, description, dueDate, priority, notes, subtasks, tags){
+    constructor(title, description, dueDate, priority){
         this.title = title,
         this.description = description,
         this.dueDate = dueDate,
         this.priority = priority,
-        this.notes = notes,
-        this.subtasks = subtasks,
-        this.tags = tags
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(newTitle) {
+        return this._title = newTitle;
+    }
+
+    get description() {
+        return this._description;
+    }
+    
+    set description(newDescription) {
+        return this._description = newDescription;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    set dueDate(newDueDate) {
+        return this._dueDate = newDueDate;
+    }
+
+    get priority () {
+        return this._priority;
+    }
+
+    set priority (newPriority) {
+        return this._priority = newPriority
     }
 }
+
+export {Task};
--- src/modules/taskHandler.js
+++ src/modules/taskHandler.js
@@ -1,2 +1,21 @@
+import { Task } from "./task";
 
 const tasks = [];
+
+function createTask() {
+    /*Press button to bring up dialog to create task*/
+    /*Initialize process of task creation*/
+};
+
+function editTaskProperties() {
+    /*Task formation - fill out fields to get task properties*/
+    
+};
+
+function addTaskToList(task) {
+    /* Appends task to array*/
+    /* task argument is a dom element filled with object info 
+        that is created from another function*/
+    const taskList = document.querySelector('.task-list');
+    taskList.append(task);
+};
